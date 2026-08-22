4c1bd6f966dcb4accd2247ddbf36cc575b08e65f
Ritika <ritikakumari87344@gmail.com>
2026-08-22 16:35:47 +0530

    NEW CHNAGES ADDED

--- src/components/all_dashbords/dis_admin/user_dashboard/UserDashBoard.jsx
+++ src/components/all_dashbords/dis_admin/user_dashboard/UserDashBoard.jsx
@@ -14,7 +14,7 @@ const steps = ["Step 1", "Step 2", "Step 3", "Step 4", "Step 5"];
 const requiredByStep = {
   0: [
     "childName", "fatherName", "motherName", "birthDate",
-    "ageYears", "ageMonths", "gender", "resident",
+    "gender", "resident",
     "permanentग्राम/मोहल्ला", "permanentडाकघर",
     "permanentविकासखण्ड/नगर निकाय", "permanentजनपद", "permanentपिन कोड",
   ],
